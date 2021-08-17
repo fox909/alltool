039a21624cd14d02272cd2e1a7bffb68f9860336
fox909 <89049956+fox909@users.noreply.github.com>
2021-08-17 03:19:04 +0300

    Add files via upload

---
+++ SDS.py
@@ -0,0 +1,134 @@
+import os
+import time
+import sys
+
+os.system("clear")
+
+print('''\033[91m
+          .                                                      .
+        .n                   .                 .                  n.
+  .   .dP                  dP                   9b                 9b.    .
+ 4    qXb         .       dX                     Xb       .        dXp     t
+dX.    9Xb      .dXb    __                         __    dXb.     dXP     .Xb
+9XXb._       _.dXXXXb dXXXXbo.                 .odXXXXb dXXXXb._       _.dXXP
+ 9XXXXXXXXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXXXXXP
+  `9XXXXXXXXXXXXXXXXXXXXX'~   ~`OOO8b   d8OOO'~   ~`XXXXXXXXXXXXXXXXXXXXXP'
+    `9XXXXXXXXXXXP' `9XX'   DIE    `98v8P'  HUMAN   `XXP' `9XXXXXXXXXXXP'
+        ~~~~~~~       9X.          .db|db.          .XP       ~~~~~~~
+                        )b.  .dbo.dP'`v'`9b.odb.  .dX(
+                      ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.
+                     dXXXXXXXXXXXP'   .   `9XXXXXXXXXXXb
+                    dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb
+                    9XXb'   `XXXXXb.dX|Xb.dXXXXX'   `dXXP
+                     `'      9XXXXXX(   )XXXXXXP      `'
+                              XXXX X.`v'.X XXXX
+                              XP^X'`b   d'`X^XX
+                              X. 9  `   '  P )X
+                              `b  `       '  d'
+                               `             '
+                   SDS TEAM 
+''')
+def slowprint(s):
+    for c in s + '\n' :
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(5. / 100)
+
+print (''' \033[93m
++--------------------------------------+
+| this tool install all basic packages |
++--------------------------------------+
+| coded by FOX => my insta @mc_fox_964 |
++--------------------------------------+''')
+
+slowprint(''' \033[93m
+[01] python             [02] python2
+[03] python-dev         [04] python3
+[05] php                [06] java
+[07] git                [08] perl
+[09] bash               [10] nano
+[11] curl               [12] openssl
+[13] openssh            [14] wget
+[15] clang              [16] nmap
+[17] w3m                [18] hydra
+[19] ruby               [20] macchanger
+[21] host               [22] dnsutils
+[23] coreutils          [24] fish
+[25] zip                [27] tor
+[28] hydra              [29] figlet 
+[30] cowsay             [31] tar
+[32] unzip              [33] vim
+[34] ruby               [35] wcalc
+[36] bmon               [37] unrar
+[38] proot              [39] golang
+[40] NsLookup''')
+slowprint('''\033[96m
+This Command for access Storage in Termux
+[00] termux-setup-storage''')
+print ("                                            ")
+choice = input("\033[93mdo you want to install all packages [y/n] : ")
+if choice == 'n' : sys.exit()
+if choice == 'y' : os.system ("apt update")
+os.system ("apt upgrade -y")
+os.system ("apt install python -y")
+os.system ("apt install python2 -y")
+os.system ("apt install php -y")
+os.system ("apt install python-dev -y")
+os.system ("apt install python3 -y")
+os.system ("apt install java -y")
+os.system ("pkg install root-repo -y")
+os.system ("pkg install unstable-repo -y")
+os.system ("pkg install x11-repo -y")
+os.system ("apt install git -y")
+os.system ("apt install perl -y")
+os.system ("apt install bash -y")
+
+print ("wait for second and start hacking")
+
+os.system ("apt install nano -y")
+os.system ("apt install curl -y")
+os.system ("apt install openssl -y")
+os.system ("apt install openssh -y")
+os.system ("apt install wget -y")
+os.system ("apt install clang -y")
+os.system ("apt install nmap -y")
+os.system ("apt install w3m -y")
+os.system ("apt install hydra -y")
+os.system ("apt install ruby2 -y")
+os.system ("apt install macchanger -y")
+os.system ("apt install host -y")
+os.system ("apt install dnsutils -y")
+os.system ("apt install coreutils -y")
+os.system ("apt install fish -y")
+os.system ("apt install zip -y")
+os.system ("apt install hydra -y")
+os.system ("apt install figlet -y")
+os.system ("apt install cowsay -y")
+os.system ("apt install unzip -y")
+os.system ("apt install vim -y")
+os.system ("apt install ruby -y")
+os.system ("apt install wcalc -y")
+os.system ("apt install bmon -y")
+os.system ("apt install unrar -y")
+os.system ("apt install proot -y")
+os.system ("apt install golang -y")
+os.system ("pkg install dnsutils -y")
+print ("allow the button for access the storage in termux")
+
+
+os.system ("termux-setup-storage")
+  
+def slowprint(s):
+    for c in s + '\n' :
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(8. / 100)
+print("\033[91m+-------------------------------------------+")
+slowprint('''\033[91m|        all hack tools are installed       |
+|           ~×- start hacking -×~           |
+|             >>> SDS TEAM <<<              |''')
+print("+-------------------------------------------+")
+
+
+
+input("\n\npress the enter key to EXIT : ")
